5421f84119be34db38716a91d3f9378a98467e6b
Dakota <bazookaharwell@gmail.com>
2017-05-11 14:07:50 -1000

    Create debug init tests

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,4 @@
+/*jshint: esverion:6*/
 const reactContainer = document.getElementById("root");
 
 // For Testing and Initialization purposes
@@ -7,12 +8,62 @@ postUser(JSON.stringify({
 }))
 .then( (user) => {
   console.log(user);
-});
-getUsers()
-.then( (users) => {
-  console.log(users);
-});
 
+  postTask(JSON.stringify({
+    title: 'Make some more CSS',
+    status: 0,
+    priority: 1,
+    created_by: 1,
+    assigned_to: 1
+  }))
+  .then( (task) => {
+    console.log(task);
+  });
+
+  postTask(JSON.stringify({
+    title: 'Make some more CSS',
+    status: 0,
+    priority: 3,
+    created_by: 1,
+    assigned_to: 1
+  }))
+  .then( (task) => {
+    console.log(task);
+  });
+
+  postTask(JSON.stringify({
+    title: 'Make some more CSS',
+    status: 1,
+    priority: 2,
+    created_by: 1,
+    assigned_to: 1
+  }))
+  .then( (task) => {
+    console.log(task);
+  });
+
+  postTask(JSON.stringify({
+    title: 'Make some more CSS',
+    status: 2,
+    priority: 1,
+    created_by: 1,
+    assigned_to: 1
+  }))
+  .then( (task) => {
+    console.log(task);
+  });
+
+  postTask(JSON.stringify({
+    title: 'Make some more CSS',
+    status: 2,
+    priority: 0,
+    created_by: 1,
+    assigned_to: 1
+  }))
+  .then( (task) => {
+    console.log(task);
+  });
+});
 // end
 
 ReactDOM.render(
