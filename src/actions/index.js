6416890b7566da388dd0036959ef6d2034a61931
Dakota <bazookaharwell@gmail.com>
2017-05-16 11:15:55 -1000

    More dispatchable actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,30 @@
+/*jshint esversion: 6*/
 export const LOAD_CARDS = 'LOAD_CARDS';
+export const ADD_CARD = 'ADD_CARD';
+export const DELETE_CARD = 'DELETE_CARD';
+export const MOVE_CARDS = 'MOVE_CARDS';
 
 export const loadCards = cards => {
   return {
     type: LOAD_CARDS,
     cards
+  };
+};
+export const addCard = card => {
+  return {
+    type: ADD_CARD,
+    card
+  };
+};
+export const destroyCard = card => {
+  return {
+    type: DELETE_CARD,
+    card
+  };
+};
+export const moveCard = card => {
+  return {
+    type: MOVE_CARD,
+    card
   };
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,19 +1,30 @@
 import {
-  LOAD_CARDS
+  LOAD_CARDS,
+  ADD_CARD,
+  DELETE_CARD,
+  MOVE_CARD
 } from '../actions';
 
 const initialState = {
   cards : []
 };
 const cards = (state = initialState, action) => {
-  console.log("state", state);
-  console.log("action", action);
-
   switch(action.type){
-    case LOAD_CARDS:
-      return Object.assign({}, state, { cards: action.cards });
-    default:
+    case LOAD_CARDS: {
+      return Object.assign({}, state, {cards: action.cards});
+    }
+    case ADD_CARD: {
+      return Object.assign({}, state, {cards: [action.card].concat(state.cards)});
+    }
+    case DELETE_CARD: {
+      return Object.assign({}, state, {cards: action.cards});
+    }
+    case MOVE_CARD: {
+      return Object.assign({}, state, {cards: [action.card].concat(state.cards)});
+    }
+    default: {
       return state;
+    }
   }
 };
 
