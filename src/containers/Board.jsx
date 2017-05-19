245b7d82a318216995060d6955654ec78d02de29
Dakota <bazookaharwell@gmail.com>
2017-05-18 14:33:49 -1000

    More CSS

--- src/containers/Board.jsx
+++ src/containers/Board.jsx
@@ -60,15 +60,17 @@ class Board extends Component {
     return (
       <div>
         <Header />
-        <div id="board-container">
-          <div id="board-queue">
-            <List status={0} cards={this.props.cards.filter( (card) => card.status === 0 )} />
-          </div>
-          <div id="board-progress">
-            <List status={1} cards={this.props.cards.filter( (card) => card.status === 1 )} />
-          </div>
-          <div id="board-done">
-            <List status={2} cards={this.props.cards.filter( (card) => card.status === 2 )} />
+        <div id="body-container">
+          <div id="board-container">
+            <div id="board-queue">
+              <List status={0} cards={this.props.cards.filter( (card) => card.status === 0 )} />
+            </div>
+            <div id="board-progress">
+              <List status={1} cards={this.props.cards.filter( (card) => card.status === 1 )} />
+            </div>
+            <div id="board-done">
+              <List status={2} cards={this.props.cards.filter( (card) => card.status === 2 )} />
+            </div>
           </div>
         </div>
         <Footer />
